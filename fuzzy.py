976b683d263631b2bc92463acf0e79b2a798b720
ack8006 <takata.alex@gmail.com>
2015-12-07 16:59:54 -0500

    venue capacity updater using fuzzy matching

---
+++ fuzzy.py
@@ -0,0 +1,93 @@
+from difflib import SequenceMatcher
+
+
+def ratio(item_one, item_two, ignore_case=False):
+    if ignore_case:
+        item_one, item_two = item_one.lower(), item_two.lower()
+    return SequenceMatcher(None, item_one, item_two).ratio()
+
+def partial_ratio(item_one, item_two, ignore_case=False):
+    if len(item_one) <= len(item_two):
+        shrt, lng = item_one, item_two
+    else:
+        shrt, lng = item_two, item_one
+    diff = len(lng)-len(shrt)
+    return max(map(lambda x: ratio(shrt, lng[x:x+len(shrt)]), xrange(diff+1)))
+
+def token_sorted_ratio(item_one, item_two, ignore_case=False):
+    item_one = ' '.join(sorted(item_one.split()))
+    item_two = ' '.join(sorted(item_two.split()))
+    return ratio(item_one, item_two, ignore_case)
+
+def token_set_ratio(item_one, item_two, ignore_case=False):
+    set_one, set_two = set(item_one.split()), set(item_two.split())
+    intersection = set_one & set_two
+    for token in intersection:
+        set_one.discard(token)
+        set_two.discard(token)
+
+    intersection = ' '.join(intersection)
+    set_one = ' '.join(set_one)
+    set_two = ' '.join(set_two)
+    #intersection vs. intersection + rest of item one
+    #intersection vs. intersection + rest of item two
+    #intersection vs. intersection + rest of both
+    return max(ratio(intersection, intersection + ' ' + set_one, ignore_case),
+               ratio(intersection, intersection + ' ' + set_two, ignore_case),
+               ratio(intersection + ' ' + set_one, intersection+' '+set_two, ignore_case))
+
+def all_techniques(item_one, item_two, ignore_case = False):
+    return max(ratio(item_one, item_two, ignore_case),
+               partial_ratio(item_one, item_two, ignore_case),
+               token_sorted_ratio(item_one, item_two, ignore_case),
+               token_set_ratio(item_one, item_two, ignore_case))
+
+def avg_all_techniques(item_one, item_two, ignore_case = False):
+    return sum([ratio(item_one, item_two, ignore_case),
+               partial_ratio(item_one, item_two, ignore_case),
+               token_sorted_ratio(item_one, item_two, ignore_case),
+               token_set_ratio(item_one, item_two, ignore_case)])/4.0
+
+#def get_best_scores(item_one, choices, limit, fn):
+#    def wrapped():
+#        match_scores = map(lambda x: (x, fn()(item_one,x)), choices)
+#        return sorted(match_scores, key = lambda x:x[1], reverse=True)[:limit]
+#    return wrapped
+
+#***This feels like it could use decorators
+#@get_best_scores(item_one, choices, limit)
+def best_match_ratio(item_one, choices, limit=1):
+    return get_best_scores(item_one, choices, limit, ratio)
+
+def best_match_partial(item_one, choices, limit=1):
+    return get_best_scores(item_one, choices, limit, partial_ratio)
+
+def best_match_token(item_one, choices, limit=1):
+    return get_best_scores(item_one, choices, limit, token_sorted_ratio)
+
+def best_match_token_set(item_one, choices, limit = 1):
+    return get_best_scores(item_one, choices, limit, token_set_ratio)
+
+def best_match_all(item_one, choices, limit = 1):
+    return get_best_scores(item_one, choices, limit, all_techniques)
+
+def best_match_avg(item_one, choices, limit = 1):
+    return get_best_scores(item_one, choices, limit, avg_all_techniques)
+
+
+def get_best_scores(item_one, choices, limit, match_function):
+    match_scores = map(lambda x: (x, match_function(item_one,x)), choices)
+    return sorted(match_scores, key = lambda x:x[1], reverse=True)[:limit]
+
+
+
+if __name__ == '__main__':
+    #print best_match_ratio('yankees', ['new york yankees', 'mets', 'knicks', 'rangers'])
+    #print best_match_partial('yankees', ['new york yankees', 'mets', 'knicks', 'rangers'], 3)
+    #print best_match_token('yankees of new york', ['new york yankees', 'mets', 'knicks', 'rangers'], 3)
+    print best_match_avg('yankees of new york', ['new york yankees', 'mets', 'knicks', 'rangers'], 3)
+
+
+
+
+
---
+++ venue_capacity_updater.py
@@ -0,0 +1,101 @@
+import requests
+from requests.exceptions import ConnectionError
+from api_keys import song_kick_api
+from dbconnection import start_db_connection
+from contextlib import closing
+import psycopg2
+#from fuzzywuzzy import fuzz, process
+import fuzzy
+from time import sleep
+
+
+
+
+#venues = sorted([x[0] for x in venues])
+#for x in xrange(len(venues)):
+#    print venues[x]
+#    response = fuzzy.best_match_token(venues[x], venues, 5)[1:]
+#    print [x for x in response if x[1]>.75]
+
+def get_venues_to_update():
+    conn = start_db_connection()
+    with closing(conn.cursor()) as cur:
+        cur.execute('''SELECT name, venue_state FROM venues
+                    WHERE sk_venue_id IS NULL''')
+        venues = cur.fetchall()
+    conn.close()
+    return venues
+
+def request_id(venues):
+    venue_dict = {}
+    for venue in venues:
+        sleep(2.5)
+        venue, city = venue
+        #print
+        print venue
+        url = make_url(venue, city)
+        try:
+            data = get_json(request_page(url))
+        except ConnectionError as e:
+            print e
+            continue
+        if not data or data['resultsPage']['totalEntries'] < 1:
+            sk_id = None
+            continue
+        else:
+            possible_venue_info = get_pos_venues(data)
+            sk_id = get_id_based_on_fuzzy(venue, city, possible_venue_info)
+        print sk_id
+        venue_dict[venue] = sk_id
+    return venue_dict
+
+def request_page(url):
+    return requests.get(url)
+
+def get_json(r):
+    return r.json()
+
+def make_url(venue, city):
+    query = venue.replace(' ','+')+'+'+city
+    url = ('http://api.songkick.com/api/3.0/search/venues.json?query=%s&'
+            'apikey=%s' %(query, song_kick_api))
+    return url
+
+def get_id_based_on_fuzzy(venue, city, possible_venue_info):
+    pos_venue_names = get_pos_venue_names(possible_venue_info)
+    fuzzy_matches = fuzzy.best_match_avg(venue, pos_venue_names.keys(),
+                                             len(pos_venue_names))
+    #print pos_venue_names
+    #print fuzzy_matches
+    #*****.80 random choice
+    for match in [x[0] for x in fuzzy_matches if x[1] > 0.8]:
+        if pos_venue_names[match][1]:
+            return pos_venue_names[match]
+    #else return best match without capacity
+    return pos_venue_names[fuzzy_matches[0][0]]
+
+def get_pos_venues(json):
+    return json['resultsPage']['results']['venue']
+
+def get_pos_venue_names(possible_venue_info):
+    return {x['displayName']: (x['id'], x['capacity']) for x in possible_venue_info}
+
+def upload_venue_capacity(venues):
+    for venue, id_cap in venues.iteritems():
+        sk_venue_id, capacity = id_cap
+        conn = start_db_connection()
+        with closing(conn.cursor()) as cur:
+            cur.execute('''UPDATE venues
+                        SET sk_venue_id=%s, capacity=%s
+                        WHERE name=%s''', (sk_venue_id, capacity, venue))
+            conn.commit()
+        conn.close()
+
+
+
+if __name__ == '__main__':
+    venues = request_id(get_venues_to_update())
+    upload_venue_capacity(venues)
+
+
+

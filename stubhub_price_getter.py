aab6bdb80e438e7ae6299d1dbcefa02beaa82c43
ack8006 <takata.alex@gmail.com>
2016-03-27 23:55:50 -0400

    updated price update query

--- stubhub_price_getter.py
+++ stubhub_price_getter.py
@@ -11,7 +11,14 @@ def get_stubhub_ids():
     with closing(conn.cursor()) as cur:
         cur.execute('''SELECT sl.stubhubid FROM event e
                     JOIN stubhub_listing sl ON e.id=sl.event_id
-                    WHERE e.event_date > now()''')
+                    JOIN
+                    (SELECT MAX(update_time), sl_id FROM stubhub_point sp
+                    GROUP BY sl_id
+                    ORDER BY sl_id) as m
+                    ON m.sl_id = sl.id
+                    WHERE e.event_date >= now() AND
+                    (e.onsale_date > (now() - interval '1 day')
+                    OR now()-m.MAX > interval '4 hours')''')
         stubhub_ids = [x[0] for x in cur.fetchall()]
     conn.close()
     print stubhub_ids
